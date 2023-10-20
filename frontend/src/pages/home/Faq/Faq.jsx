ce62ffee4babcc23c769cc0eb2f3ee751c39d8d4
gcWDev <cohenguy74@gmail.com>
2023-10-20 19:47:15 -0400

    made accordions two column

--- frontend/src/pages/home/Faq/Faq.jsx
+++ frontend/src/pages/home/Faq/Faq.jsx
@@ -24,8 +24,6 @@ export default function Faq() {
             content="To join a study group, simply fill out the form with your preferences and a list of suitable study groups will be displayed. You can then select the group you want to join."
           />
         </div>
-      </div>
-      <div className="row justify-content-center my-3">
         <div className="col-6">
           <Accordion
             setId={setId}
@@ -48,8 +46,6 @@ export default function Faq() {
             content="As of now, there is no limit to the number of groups you can join. However, we encourage you to only join groups you are genuinely interested in."
           />
         </div>
-      </div>
-      <div className="row justify-content-center my-3">
         <div className="col-6">
           <Accordion
             setId={setId}
@@ -72,8 +68,6 @@ export default function Faq() {
             content="You can find a wide range of hobbies from sports, cooking, coding, to music and arts. Just fill out your interests in the form and suitable groups will be suggested."
           />
         </div>
-      </div>
-      <div className="row justify-content-center my-3">
         <div className="col-6">
           <Accordion
             setId={setId}
