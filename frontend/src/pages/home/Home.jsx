c2d3d5c3e2134d1852c49b967003d41a6c697b7f
gcWDev <cohenguy74@gmail.com>
2023-10-20 20:15:46 -0400

    added school logos

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import About from "./About/About";
 import Faq from "./Faq/Faq";
 import Hero from "./Hero/Hero";
+import Marquee from "./Marquee/Marquee";
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
       <Faq />
+      <Marquee />
     </>
   );
 }
