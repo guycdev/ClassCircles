401d0de9a8c21ccfef12810855e125f2409fbea9
mattand <mjande@umich.edu>
2023-10-21 11:10:22 -0400

    Add skeleton for login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,11 +8,13 @@ import {
 import HomeLayout from "./layouts/HomeLayout";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/home/Home";
+import LogIn from "./pages/auth/login/Login";
 
 const route = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<HomeLayout />}>
       <Route index element={<Home />} />
+      <Route path="log-in" element={<LogIn />} />
     </Route>
   )
 );
